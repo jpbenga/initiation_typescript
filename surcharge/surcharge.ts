0a35de3c9abe12099822fcd08448b849ad735050
jpbenga <jpbenga@gmail.com>
2023-09-25 11:45:36 +0200

    surcharge

--- surcharge/surcharge.ts
+++ surcharge/surcharge.ts
@@ -1,39 +1,17 @@
 //toDate(1,1,2023)
 
-function toDate(jour: number, mois: number, annee: number):Date{
-    const date = new Date(annee, mois, jour)
-    console.log(date)
-    return date
-}
+function toDate(jour: number, mois: number, annee: number): Date
 
-function toDate(jour: number):Date{
-    const date = new Date(jour)
-    console.log(date)
-    return date
-}
+function toDate(jour: number): Date 
 
-function toDate(jour: number, mois?: number, annee?: number): Date | undefined {
+function toDate(jour: number, mois?: number, annee?: number): Date {
     if (mois !== undefined && annee !== undefined) {
-        const date = new Date(annee, mois - 1, jour);
+        const date = new Date(annee, mois, jour);
         console.log(date);
         return date;
-    } else if (jour !== undefined) {
+    } else {
         const date = new Date(jour);
         console.log(date);
         return date;
     }
-    return undefined;
 }
-
-// function toDate(jour: number, mois?: number, annee?: number, ):Date|undefined{
-//     if(jour !== undefined){
-//         const date = new Date(jour)
-//         console.log(date)
-//         return date
-//     }else if(mois !== undefined && annee !== undefined){
-//         const date = new Date(annee, mois, jour)
-//         console.log(date)
-//         return date
-//         }
-//         return undefined
-//     }
